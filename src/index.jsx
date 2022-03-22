84eee6755f6920145ec7f33f0693d765dd378875
Mikael Peltomaa <mikael.peltomaa@ksyk.fi>
2022-03-22 15:30:28 +0200

    react

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import './assets/stylesheets/style.css'
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>Hello World!</div>
+  <div>Hmm ainaki joku toimii</div>
 );
 
 class App extends Component {
